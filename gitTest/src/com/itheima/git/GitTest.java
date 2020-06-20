0fdfaa83a70a48de76d2dae30243f9a1ad8cd199
YJJ <2383394442@qq.com>
2020-06-20 13:03:37 +0800

    IDEA commit

---
+++ gitTest/src/com/itheima/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.itheima.git;
+
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git idea！！！");
+        System.out.println("hello git idea！！！");
+        System.out.println("hello git idea！！！");
+        System.out.println("hello git idea！！！");
+        System.out.println("hello git idea！！！");
+        System.out.println("hello git idea！！！");
+        System.out.println("hello git idea！！！");
+    }
+
+}
