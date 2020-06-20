4b0e2a824994b28ee6946969c16286b072d9474e
YJJ <2383394442@qq.com>
2020-06-20 13:14:06 +0800

    克隆+修改+push

--- gitTest/src/com/itheima/git/GitTest.java
+++ gitTest/src/com/itheima/git/GitTest.java
@@ -5,11 +5,14 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git idea！！！");
         System.out.println("hello git idea！！！");
-        System.out.println("hello git idea！！！");
-        System.out.println("hello git idea！！！");
-        System.out.println("hello git idea！！！");
-        System.out.println("hello git idea！！！");
-        System.out.println("hello git idea！！！");
+
+        System.out.println("clone and 修改");
+        //修改再push
+    }
+
+    public void test() {
+        int i = 1;
+        System.out.println(i);
     }
 
 }
